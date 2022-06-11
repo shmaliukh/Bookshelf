55a43f672c1a1367c55bc39bbfe6ee1c270110ae
zskat02@gmail.com <zSkAt_02>
2022-06-11 11:53:53 +0300

    v 0.1.1 Terminal mod + ability to add new book or magazine ( + random add)

--- src/main/java/org/ShmaliukhVlad/Main.java
+++ src/main/java/org/ShmaliukhVlad/Main.java
@@ -8,53 +8,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("1",1,false,"NoAutho1",new Date(1));
-        Book book2 = new Book("2",2,false,"NoAuthor2",new Date(2));
-        Book book3 = new Book("3",3,true,"NoAuthor3",new Date(3));
+        Terminal terminal = new Terminal();
+        terminal.startWork();
 
-        Magazine magazine1 = new Magazine("4",4,false);
-        Magazine magazine2 = new Magazine("5",5,false);
-        Magazine magazine3 = new Magazine("6",6,true);
-
-        Shelf shelf = new Shelf();
-
-        shelf.addLiteratureObject(book1);
-        shelf.addLiteratureObject(book2);
-        shelf.addLiteratureObject(book3);
-
-        shelf.addLiteratureObject(magazine1);
-        shelf.addLiteratureObject(magazine2);
-        shelf.addLiteratureObject(magazine3);
-
-        System.out.println(shelf);
-
-        shelf.saveShelfToFile();
-
-
-
-
-
-        //ArrayList<Literature> literatureInShelf = new ArrayList<>();
-//
-        //Book book1 = new Book("1",1,false,"NoAutho1",new Date(1));
-        //Book book2 = new Book("2",2,false,"NoAuthor2",new Date(2));
-        //Book book3 = new Book("3",3,false,"NoAuthor3",new Date(3));
-//
-        //literatureInShelf.add(book3);
-        //literatureInShelf.add(book1);
-        //literatureInShelf.add(book2);
-//
-//
-        //System.out.println(literatureInShelf);
-//
-        //System.out.println("Сортування за назвою");
-        //System.out.println("Sort by name");
-        //literatureInShelf.sort((s1, s2) -> {
-        //    if(s1.getName().length() != s2.getName().length())
-        //        return s1.getName().length() - s2.getName().length();
-        //    else
-        //        return s1.compareTo(s2);
-        //});
-        //System.out.println(literatureInShelf);
     }
 }
--- src/main/java/org/ShmaliukhVlad/Terminal.java
+++ src/main/java/org/ShmaliukhVlad/Terminal.java
@@ -1,14 +1,26 @@
 package org.ShmaliukhVlad;
 
+import org.ShmaliukhVlad.Bookshelf.Bookshelf_objects.Book;
+import org.ShmaliukhVlad.Bookshelf.Bookshelf_objects.Magazine;
+import org.ShmaliukhVlad.Bookshelf.Shelf;
+
 import java.awt.desktop.AboutEvent;
+import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
+enum MenuLevel{
+
+}
 
 public class Terminal {
     private Scanner scanner;
     private int userChoice;
+    private Shelf shelf;
 
     {
         scanner = new Scanner(System.in);
+        shelf = new Shelf();
     }
 
     public Terminal(){
@@ -16,40 +28,117 @@ public class Terminal {
 
     public void startWork(){
         boolean play = true;
+        int userChoice;
 
         while (true){
             printMainMenu();
-            getUserChoice();
+            userChoice = getUserChoice();
+            if (userChoice == 1){
+                System.out.println("Choose type of literature you want to add:");
+                System.out.println("1 - Magazine");
+                System.out.println("2 - Book");
+                System.out.println("3 - Random Magazine");
+                System.out.println("4 - Random Book");
+                System.out.println("0 - Back");
+
+                userChoice = getUserChoice();
+
+                if(userChoice == 1){
+                    Magazine userMagazineToAdd;
+                    String name = "";
+                    int pages = 0;
+                    boolean isBorrowed = false;
+
+                    System.out.println("Enter name:");
+                    name = scanner.next();
+                    System.out.println("Enter number of pages:");
+                    pages = scanner.nextInt();
+                    System.out.println("Enter 1 if Magazine is borrowed:");
+                    System.out.println("Enter 2 if Magazine is NOT borrowed:");
+                    if(getUserChoice() == 1){
+                        isBorrowed = true;
+                    }
+
+                    userMagazineToAdd = new Magazine(name, pages, isBorrowed);
+                    shelf.addLiteratureObject(userMagazineToAdd);
+                    System.out.println("New user Magazine has added to shelf");
+
+                }
+                else if(userChoice == 2){
+                    Book userBookToAdd;
+                    String name = "";
+                    int pages = 0;
+                    boolean isBorrowed = false;
+                    String author = "";
+                    Date dateOfIssue = new Date(0);
+
+                    System.out.println("Enter name:");
+                    name = scanner.next();
+                    System.out.println("Enter number of pages:");
+                    pages = scanner.nextInt();
+                    System.out.println("Enter 1 if Book is borrowed:");
+                    System.out.println("Enter 2 if Book is NOT borrowed:");
+                    if(getUserChoice() == 1){
+                        isBorrowed = true;
+                    }
+                    System.out.println("Enter author:");
+                    author = scanner.nextLine();
+                    //Todo
+                    System.out.println("Enter date of issue:");
+                    dateOfIssue = new Date(scanner.nextInt());
+
+                    userBookToAdd = new Book(name, pages, isBorrowed, author, dateOfIssue);
+                    shelf.addLiteratureObject(userBookToAdd);
+                    System.out.println("New user Book has added to shelf");
+                }
+                else if(userChoice == 3){
+                    Random randomNumber = new Random();
+
+                    shelf.addLiteratureObject(new Magazine(
+                            getRandomString(randomNumber.nextInt(10)),
+                            randomNumber.nextInt(1000),
+                            false));
+                    System.out.println("New random Magazine has added to shelf");
+                }
+                else if(userChoice == 4){
+                    Random randomNumber = new Random();
+
+                    shelf.addLiteratureObject(new Book(
+                            getRandomString(randomNumber.nextInt(10)),
+                            randomNumber.nextInt(1000),
+                            false,
+                            getRandomString(randomNumber.nextInt(10)),
+                            new Date(randomNumber.nextInt(10000))));
+                    System.out.println("New random Book has added to shelf");
+                }
+                else if(getUserChoice() == 0){
+
+                }
+                System.out.println("Shelf state:");
+                System.out.println(shelf);
+            }
+
+
 
 
 
         }
     }
-
-
-
-    private void getUserChoice(){
-
-        userChoice = scanner.nextShort();
-
-        if (userChoice == 1) {
-
-        } else if (userChoice == 2) {
-
-        } else if (userChoice == 3) {
-
-        } else if (userChoice == 4) {
-
-        } else if (userChoice == 5) {
-
-        } else if (userChoice == 6) {
-
-        } else if (userChoice == 7) {
-
+    public String getRandomString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(62);
+            sb.append(str.charAt(number));
         }
-        else {
-            System.out.println("Було введенно значення не вірної команди");
-        }
+        return sb.toString();
+    }
+
+
+
+    private int getUserChoice(){
+        return scanner.nextInt();
     }
 
     private void printMainMenu(){
