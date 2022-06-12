52b703db7b23d42820ae921910239fdf3be35553
Vlad Shmaliukh <zskat02@gmail.com>
2022-06-12 19:29:37 +0300

    v 1.0.1 tests refactor

--- src/test/java/org/ShmaliukhVlad/Bookshelf_objects/BookTest.java
+++ src/test/java/org/ShmaliukhVlad/Bookshelf_objects/BookTest.java
@@ -1,120 +1,111 @@
 package org.ShmaliukhVlad.Bookshelf_objects;
 
 import org.ShmaliukhVlad.Bookshelf.Bookshelf_objects.Book;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
     @Test
+    @DisplayName("test getName")
     void getName() {
-        System.out.printf( "%s%-30s" ,"test ", "getName ");
         String expectedName = "someName";
         Book book1 = new Book("someName",1,false,"NoAuthor1", LocalDate.now());
 
         assertEquals(book1.getName(), expectedName);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test setName")
     void setName() {
-        System.out.printf( "%s%-30s" ,"test ", "setName ");
         String expectedStr = "someName";
         Book book1 = new Book("1",1,false,"NoAuthor1",LocalDate.now());
         book1.setName("someName");
 
         assertEquals(book1.getName(), expectedStr);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test getPagesNumber")
     void getPagesNumber() {
-        System.out.printf( "%s%-30s" ,"test ", "getPagesNumber ");
         int expectedNumber = 1;
         Book book1 = new Book("someName",1,false,"NoAuthor1",LocalDate.now());
 
         assertEquals(book1.getPagesNumber(), expectedNumber);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test  setPagesNumber (1)")
     void setPagesNumber_0() {
-        System.out.printf( "%s%-30s" ,"test ", "setPagesNumber(1) ");
         int expectedNumber = 1;
         Book book1 = new Book("someName",0,false,"NoAuthor1",LocalDate.now());
         book1.setPagesNumber(1);
 
         assertEquals(book1.getPagesNumber(), expectedNumber);
-        System.out.println("is successful");
     }
     @Test
+    @DisplayName("test  setPagesNumber (-1)")
     void setPagesNumber_1() {
-        System.out.printf( "%s%-30s" ,"test ", "setPagesNumber(-1) ");
         int expectedNumber = 0;
         Book book1 = new Book("someName",0,false,"NoAuthor1",LocalDate.now());
         book1.setPagesNumber(-1);
 
         assertEquals(book1.getPagesNumber(), expectedNumber);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test isBorrowed")
     void isBorrowed() {
-        System.out.printf( "%s%-30s" ,"test ", "isBorrowed ");
         boolean expectedBoolean = false;
         Book book1 = new Book("someName",1,false,"NoAuthor1",LocalDate.now());
 
         assertEquals(book1.isBorrowed(), expectedBoolean);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test setBorrowed")
     void setBorrowed() {
-        System.out.printf( "%s%-30s" ,"test ", "setBorrowed ");
         boolean expectedBoolean = true;
         Book book1 = new Book("someName",0,false,"NoAuthor1",LocalDate.now());
         book1.setBorrowed(true);
 
         assertEquals(book1.isBorrowed(), expectedBoolean);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test getAuthor")
     void getAuthor() {
-        System.out.printf( "%s%-30s" ,"test ", "getAuthor ");
         String expectedAuthor = "someAuthor";
         Book book1 = new Book("someName",1,false,"someAuthor",LocalDate.now());
 
         assertEquals(book1.getAuthor(),  expectedAuthor);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test setAuthor")
     void setAuthor() {
-        System.out.printf( "%s%-30s" ,"test ", "setAuthor ");
         String expectedAuthor = "someAuthor";
         Book book1 = new Book("1",1,false,"NoAuthor1",LocalDate.now());
         book1.setAuthor("someAuthor");
 
         assertEquals(book1.getAuthor(), expectedAuthor);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test getIssuanceDate")
     void getIssuanceDate() {
-        System.out.printf( "%s%-30s" ,"test ", "getIssuanceDate ");
         LocalDate expectedDate = LocalDate.now();
         Book book1 = new Book("someName",1,false,"someAuthor",LocalDate.now());
 
         assertEquals(book1.getIssuanceDate(),  expectedDate);
-        System.out.println("is successful");
     }
 
     @Test
+    @DisplayName("test setIssuanceDate")
     void setIssuanceDate() {
         System.out.printf( "%s%-30s" ,"test ", "setIssuanceDate ");
         LocalDate expectedDate = LocalDate.now();
@@ -123,9 +114,5 @@ class BookTest {
 
         assertEquals(book1.getIssuanceDate(), expectedDate);
         System.out.println("is successful");
-    }
-
-    @Test
-    void compareTo() {
     }
 }
