b7758fa508d50fc5abcd1c21fc1320d1e13ab1c3
ShmaliukhVlad <forkmasha100@gmail.com>
2022-07-01 17:21:01 +0300

    fix for args input in Main

--- src/main/java/org/ShmaliukhVlad/Main.java
+++ src/main/java/org/ShmaliukhVlad/Main.java
@@ -3,20 +3,27 @@ package org.ShmaliukhVlad;
 import java.io.IOException;
 import java.text.ParseException;
 
-
 /**
  * @author ShmaliukhVlad
  * @version 1.3.0
  * This is Main class for starting project
  */
 public class Main {
-
     /**
      * Here is start point of a program
      * @param args command line values
      */
-    public static void main(String[] args) throws ParseException, IOException, ClassNotFoundException {
+    public static void main(String[] args) throws ParseException, IOException{
         Terminal terminal = new Terminal();
-        terminal.startWork(Integer.parseInt(args[0]));
+        int typeOfConfig;
+
+        if(args.length>0){
+            typeOfConfig = Integer.parseInt(args[0]);
+        }
+        else{
+            typeOfConfig = 0;
+        }
+
+        terminal.startWork(typeOfConfig);
     }
 }
