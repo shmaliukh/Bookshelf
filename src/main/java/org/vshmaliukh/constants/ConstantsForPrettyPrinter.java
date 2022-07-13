77c589e7371b58e96d0e6df06b964e5109087007
shmaliukh <zskat02@gmail.com>
2022-07-13 14:35:56 +0300

    use uppercase for table titles

--- src/main/java/org/vshmaliukh/constants/ConstantsForPrettyPrinter.java
+++ src/main/java/org/vshmaliukh/constants/ConstantsForPrettyPrinter.java
@@ -5,12 +5,12 @@ public final class ConstantsForPrettyPrinter {
     private ConstantsForPrettyPrinter(){}
 
     public static final String TITLE_INDEX = "#";
-    public static final String TITLE_TYPE = "Type";
-    public static final String TITLE_NAME = "Name";
-    public static final String TITLE_PAGES = "Pages";
-    public static final String TITLE_BORROW = "Borrow";
-    public static final String TITLE_AUTHOR = "Author";
-    public static final String TITLE_DATE = "Date";
+    public static final String TITLE_TYPE = "TYPE";
+    public static final String TITLE_NAME = "NAME";
+    public static final String TITLE_PAGES = "PAGES";
+    public static final String TITLE_BORROW = "BORROW";
+    public static final String TITLE_AUTHOR = "AUTHOR";
+    public static final String TITLE_DATE = "DATE";
 
     public static final String BOOK_TYPE = "Book";
     public static final String MAGAZINE_TYPE = "Magazine";
