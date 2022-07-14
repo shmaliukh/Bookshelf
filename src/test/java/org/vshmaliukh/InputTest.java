4b9697c8721398b8c41f7e407b0665729deeba1a
shmaliukh <zskat02@gmail.com>
2022-07-14 13:19:32 +0300

    clean test class (according to sonarLint rules)

--- src/test/java/org/vshmaliukh/InputTest.java
+++ src/test/java/org/vshmaliukh/InputTest.java
@@ -16,13 +16,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.vshmaliukh.constants.ConstantsForTerminal.DATE_FORMAT;
 import static org.vshmaliukh.constants.ConstantsForUserInputHandler.*;
 
-public class InputTest {
-    Scanner scanner = new Scanner(System.in);
-    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out), true);
+class InputTest {
 
-    UserInputHandler userInputHandler = new UserInputHandler(scanner, printWriter);
+    private Scanner scanner = new Scanner(System.in);
+    private final PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out), true);
+    private UserInputHandler userInputHandler = new UserInputHandler(scanner, printWriter);
 
-    String entersForRecursion;
+    private final String entersForRecursion;
+
     {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < MAX_RECURSION_LEVEL; i++) {
@@ -30,8 +31,6 @@ public class InputTest {
         }
         entersForRecursion = stringBuilder.toString();
     }
-
-
 
     @DisplayName("validate data from user input (date) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}'' expected state ''{1}''")
@@ -52,14 +51,14 @@ public class InputTest {
             "  01 01 2022   |  false" + "\n" +
             "  01/01/2022   |  false" + "\n" +
             "  01_01_2022   |  false" )
-    public void testDateValidation(String input, boolean expectedBoolean) {
+    void testDateValidation(String input, boolean expectedBoolean) {
         assertEquals(expectedBoolean, userInputHandler.isValidInputDate(input.trim(), DATE_FORMAT));
     }
 
     @DisplayName("test user input for name of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}'' expected string ''{1}''")
     @CsvFileSource(resources = "/inputName.csv", numLinesToSkip = 1)
-    public void testNameInput(String input, String expected){
+    void testNameInput(String input, String expected){
         userInputHandler = new UserInputHandler(new Scanner(input), printWriter);
         assertEquals(expected, userInputHandler.getUserLiteratureName());
     }
@@ -67,7 +66,7 @@ public class InputTest {
     @DisplayName("test for user input for date of issue")
     @ParameterizedTest(name = "{index} ==> input string ''{0}'' expected result ''{1}''")
     @MethodSource("providedStringForDateInput")
-    public void testDateInput(String  expectedOut, String input) throws ParseException {
+    void testDateInput(String  expectedOut, String input) throws ParseException {
         userInputHandler = new UserInputHandler(new Scanner(input), printWriter);
         DATE_FORMAT.setLenient(false);
         Date currentDate = userInputHandler.getUserDateOfIssue();
@@ -90,7 +89,7 @@ public class InputTest {
     @DisplayName("test user input for choosing state (author) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}'' expected string ''{0}''")
     @CsvFileSource(resources = "/inputAuthorsForValidation.csv", numLinesToSkip = 1)
-    public void testAuthorInput(String input){
+    void testAuthorInput(String input){
         userInputHandler = new UserInputHandler(new Scanner(input), printWriter);
         assertEquals(input, userInputHandler.getUserLiteratureName());
     }
@@ -98,54 +97,54 @@ public class InputTest {
     @DisplayName("validate data from user input (name) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @CsvFileSource(resources = "/inputNameForValidation.csv", numLinesToSkip = 1)
-    public void testIsNameValidate_true(String input){
+    void testIsNameValidate_true(String input){
         assertTrue(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_NAME));
     }
 
     @DisplayName("validate data from user input (is borrowed) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @CsvFileSource(resources = "/inputIsBorrowedValid.csv", numLinesToSkip = 1)
-    public void testIsBorrowedValidation_true(String input){
+    void testIsBorrowedValidation_true(String input){
         assertTrue(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_IS_BORROWED));
     }
 
     @DisplayName("validate data from user input (is borrowed) of Literature object after user Enter")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @ValueSource(strings = {"\nn", "\nN", "\nY", "\ny", "\n n", "\n\tn", "\n\nn",})
-    public void testIsBorrowedValidation_trueAfterSomeEnter(String input){
+    void testIsBorrowedValidation_trueAfterSomeEnter(String input){
         assertTrue(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_IS_BORROWED));
     }
 
     @DisplayName("validate data from user input (is borrowed) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @ValueSource(strings = {"", "  ", "c", "1", "0", "\n", "nn", "NN", "n n", "_n", "Nn"})
-    public void testIsBorrowedValidation_false(String input){
+    void testIsBorrowedValidation_false(String input){
         assertFalse(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_IS_BORROWED));
     }
 
     @DisplayName("validate data from user input (pages number) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @ValueSource(strings = {"1", " 1", "1 ", " 1 ", "\n1", "2147483647", "1000"})
-    public void testPagesValidation_true(String input){
+    void testPagesValidation_true(String input){
         assertTrue(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_PAGES));
     }
 
     @DisplayName("validate data from user input (pages number) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @ValueSource(strings = {"", "  ", "0", "c", "_1", "o", "\n", "*1", "+1", "-1", "1 000", "0", "1.2", "six", "13/1", "2,147,483,647", "-1321"})
-    public void testPagesValidation_false(String input){
+    void testPagesValidation_false(String input){
         assertFalse(userInputHandler.isValidInputInteger(input.trim(), PATTERN_FOR_PAGES));
     }
 
     @DisplayName("validate data from user input (author) of Literature object")
     @ParameterizedTest(name = "{index} ==> input string ''{0}''")
     @CsvFileSource(resources = "/inputAuthorsForValidation.csv", numLinesToSkip = 1)
-    public void testAuthorValidation_true(String input){
+    void testAuthorValidation_true(String input){
         assertTrue(userInputHandler.isValidInputString(input.trim(), PATTERN_FOR_AUTHOR));
     }
 
     @Test
-    public void testRecursion_wrongInputMaxTimesForLiteraturePages(){
+    void testRecursion_wrongInputMaxTimesForLiteraturePages(){
         scanner = new Scanner(entersForRecursion);
         userInputHandler = new UserInputHandler(scanner, printWriter);
 
@@ -153,7 +152,7 @@ public class InputTest {
     }
 
     @Test
-    public void testRecursion_wrongInputMaxTimesForLiteratureBoolean(){
+    void testRecursion_wrongInputMaxTimesForLiteratureBoolean(){
         scanner = new Scanner(entersForRecursion);
         userInputHandler = new UserInputHandler(scanner, printWriter);
 
@@ -161,7 +160,7 @@ public class InputTest {
     }
 
     @Test
-    public void testRecursion_wrongInputMaxTimesForLiteratureDate() throws ParseException {
+    void testRecursion_wrongInputMaxTimesForLiteratureDate() throws ParseException {
         scanner = new Scanner(entersForRecursion);
         userInputHandler = new UserInputHandler(scanner, printWriter);
 
@@ -169,7 +168,7 @@ public class InputTest {
     }
 
     @Test
-    public void testRecursion_wrongInputMaxTimesForLiteratureName(){
+    void testRecursion_wrongInputMaxTimesForLiteratureName(){
         scanner = new Scanner(entersForRecursion);
         userInputHandler = new UserInputHandler(scanner, printWriter);
 
@@ -177,7 +176,7 @@ public class InputTest {
     }
 
     @Test
-    public void testRecursion_wrongInputMaxTimesForUserName(){
+    void testRecursion_wrongInputMaxTimesForUserName(){
         scanner = new Scanner(entersForRecursion);
         userInputHandler = new UserInputHandler(scanner, printWriter);
 
