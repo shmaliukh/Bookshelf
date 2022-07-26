28cc304a9ae03b992ed47d66595fb18145f47cc8
shmaliukh <zskat02@gmail.com>
2022-07-26 14:36:44 +0300

    convertors use map of string tittle + value of item

---
+++ src/main/java/org/vshmaliukh/services/print_table_service/convertors/Titles.java
@@ -0,0 +1,14 @@
+package org.vshmaliukh.services.print_table_service.convertors;
+
+public class Titles {
+
+    private Titles() {
+    }
+
+    static final String TYPE = "TYPE";
+    static final String NAME = "NAME";
+    static final String PAGES = "PAGES";
+    static final String BORROWED = "BORROWED";
+    static final String AUTHOR = "AUTHOR";
+    static final String DATE = "DATE";
+}
