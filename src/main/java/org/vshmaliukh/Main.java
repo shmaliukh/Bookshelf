e09d7054a6bb8cd329b17b06123ae15dba59a7d2
Vlad Shmaliukh <zskat02@gmail.com>
2022-08-30 13:28:28 +0300

    Cleaned up main class

--- src/main/java/org/vshmaliukh/Main.java
+++ src/main/java/org/vshmaliukh/Main.java
@@ -2,12 +2,10 @@ package org.vshmaliukh;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.LifecycleException;
+import org.vshmaliukh.client_server_app.ShelfClientServerApp;
 import org.vshmaliukh.client_server_app.server.MultithreadedSocketServer;
-import org.vshmaliukh.console_terminal_app.ConsoleUI;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
 @Slf4j
 public class Main {
@@ -18,61 +16,25 @@ public class Main {
     public static final String SERVER_CLIENT_TYPE = "server-client";
     public static final String WEB_TYPE = "web";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException, IOException {
         String typeOfWork = System.getenv(APP_TYPE_OF_WORK_ENV);
         runAppByTypeOfWork(typeOfWork);
     }
 
-    static void runAppByTypeOfWork(String typeOfWork) {
+    static void runAppByTypeOfWork(String typeOfWork) throws LifecycleException, IOException {
         switch (typeOfWork.toLowerCase()) {
             case CONSOLE_TYPE:
-                TerminalThread terminalThread = new TerminalThread();
-                terminalThread.start();
+                ShelfClientServerApp.main(null);
                 break;
             case SERVER_CLIENT_TYPE:
-                ServerThread serverThread = new ServerThread();
-                serverThread.start();
+                MultithreadedSocketServer.main(null);
                 break;
             case WEB_TYPE:
-                try {
-                    ShelfWebApp.main(null);
-                } catch (LifecycleException e) {
-                    throw new RuntimeException(e);
-                }
+                ShelfWebApp.main(null);
                 break;
             default:
-                terminalThread = new TerminalThread();
-                terminalThread.start();
+                ShelfClientServerApp.main(null);
                 break;
-        }
-
-    }
-
-    static class TerminalThread extends Thread {
-        private final Scanner scanner = new Scanner(System.in);
-        private final PrintWriter printWriter = new PrintWriter(System.out, true);
-        private final ConsoleUI consoleTerminal = new ConsoleUI(scanner, printWriter);
-
-        @Override
-        public synchronized void run() {
-            try {
-                consoleTerminal.startWork(false);
-            } catch (Exception e) {
-                log.error("[TerminalThread] problem to start thread. Exception: ", e);
-            }
-        }
-    }
-
-    static class ServerThread extends Thread {
-        MultithreadedSocketServer multithreadedSocketServer = new MultithreadedSocketServer();
-
-        @Override
-        public synchronized void run() {
-            try {
-                multithreadedSocketServer.start();
-            } catch (IOException e) {
-                log.error("[ServerThread] problem to start thread. Exception: ", e);
-            }
         }
     }
 }
