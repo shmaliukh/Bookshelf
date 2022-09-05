f576fadd181222d931a85dc39ff3f68b8940f90d
vshmaliukh <zskat02@gmail.com>
2022-09-05 18:06:44 +0300

    Fixed console app module

---
+++ console-app-module/src/main/java/org/vshmaliukh/Main.java
@@ -0,0 +1,9 @@
+package org.vshmaliukh;
+
+import org.vshmaliukh.console_terminal_app.ShelfConsoleApp;
+
+public class Main {
+    public static void main(String[] args) {
+        ShelfConsoleApp.main(args);
+    }
+}
