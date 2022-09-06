b606ff719414837fd47a914862152ca10e69f122
vshmaliukh <zskat02@gmail.com>
2022-09-06 12:08:54 +0300

    Start to refactor telnet client code
    Added methods for client start configuration
    Added logs for setting up socket

--- client-app-module/src/main/java/org/vshmaliukh/client/TelnetClient.java
+++ client-app-module/src/main/java/org/vshmaliukh/client/TelnetClient.java
@@ -1,27 +1,35 @@
 package org.vshmaliukh.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+@Slf4j
 public class TelnetClient {
 
-    public static final int SOCKET_PORT_NUMBER = 8080;
+    public static final String CLIENT_HOST_ENV = "CLIENT_HOST";
+    public static final String CLIENT_PORT_ENV = "CLIENT_PORT";
 
-    private final Socket socket;
+    private static final String DEFAULT_CLIENT_SOCKET_HOST = "localhost";
+    private static final int DEFAULT_CLIENT_SOCKET_PORT = 8080;
 
-    private final Scanner socketScanner;
-    private final Scanner localScanner;
-    private final PrintWriter socketPrintWriter;
-    private final PrintWriter localPrintWriter;
+    private Socket socket;
 
-    private final Thread inThread;
-    private final Thread outThread;
+    private Scanner socketScanner;
+    private Scanner localScanner;
+    private PrintWriter socketPrintWriter;
+    private PrintWriter localPrintWriter;
+
+    private Thread inThread;
+    private Thread outThread;
 
     public TelnetClient() throws IOException {
 
-        socket = new Socket("localhost", SOCKET_PORT_NUMBER); // TODO use ENV for configuration socket
+        setUpSocket();
 
         socketScanner = new Scanner(socket.getInputStream());
         localScanner = new Scanner(System.in);
@@ -35,6 +43,49 @@ public class TelnetClient {
 
         inThread.start();
         outThread.start();
+    }
+
+    void setUpSocket() {
+        String host = setUpHost();
+        int port = setUpPort();
+        try {
+            socket = new Socket(host, port);
+        } catch (IOException ioe) {
+            log.error("[Client] Problem to set up socket: " + System.lineSeparator() +
+                    "   host = " + host + System.lineSeparator() +
+                    "   port = " + port + System.lineSeparator(), ioe);
+        }
+    }
+
+    private static String setUpHost() {
+        String host;
+        String hostStrFromEnv = System.getenv(CLIENT_HOST_ENV);
+        log.info("[Client] host = ");
+        if (StringUtils.isNotBlank(hostStrFromEnv)) {
+            host = hostStrFromEnv;
+            log.info(host + " // got from system environment variables by '" + CLIENT_HOST_ENV + "' name");
+        } else {
+            host = DEFAULT_CLIENT_SOCKET_HOST;
+            log.info(host + " // got from default value");
+        }
+        return host;
+    }
+
+    private static int setUpPort() {
+        int port = 0;
+        String portStrFromEnv = System.getenv(CLIENT_PORT_ENV);
+        if (StringUtils.isNotBlank(portStrFromEnv)) {
+            try {
+                port = Integer.parseInt(portStrFromEnv);
+                log.info(port + " // got from system environment variables by '" + CLIENT_PORT_ENV + "' name");
+            } catch (NumberFormatException nfe) {
+                log.error("problem to parse 'port' value got from system environment variables by '" + CLIENT_PORT_ENV + "' name", nfe);
+            }
+        } else {
+            port = DEFAULT_CLIENT_SOCKET_PORT;
+            log.info(port + " // got from default value");
+        }
+        return port;
     }
 
     public static void main(String[] args) throws IOException {
