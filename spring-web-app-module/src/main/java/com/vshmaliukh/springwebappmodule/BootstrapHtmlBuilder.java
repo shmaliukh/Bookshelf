fb8d4c100c3ae0faa025fea5e13f6fb425c3bfc6
vshmaliukh <zskat02@gmail.com>
2022-09-14 12:40:20 +0300

    Added ability to interact with choosing type of work page Created util class for generating Html text Added variety of header styles

---
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/BootstrapHtmlBuilder.java
@@ -0,0 +1,64 @@
+package com.vshmaliukh.springwebappmodule;
+
+public final class BootstrapHtmlBuilder {
+
+    public static final String SUBMIT_BUTTON_DEFAULT_TEXT = "Submit";
+
+    private BootstrapHtmlBuilder() {
+    }
+
+    public static String description(String str) {
+        return "<p class=\"lead\">" + str + "</p>";
+    }
+
+    public static String radioButton(String label, String id, String name, boolean checked) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<div class=\"form-check\">\n")
+                .append("<input id=\"").append(id)
+                .append("\" name=\"").append(name)
+                .append("\" type=\"radio\" class=\"form-check-input\" value=\"" + id + "\"");
+        if (checked) {
+            sb.append(" checked=\"\" ");
+        }
+        sb.append("required=\"\">\n")
+                .append("<label class=\"form-check-label\" ")
+                .append("for=\"").append(id).append("\">")
+                .append(label)
+                .append("</label>\n")
+                .append("</div>");
+        return sb.toString();
+    }
+
+//    public static String checkBox(){
+//        return "<div class=\"form-check\">\n" +
+//                "        <input type=\"checkbox\" class=\"form-check-input\" id=\"same-address\">\n" +
+//                "        <label class=\"form-check-label\" for=\"same-address\">Shipping address is the same as my billing address</label>\n" +
+//                "    </div>";
+//    }
+
+    public static String formSubmitButton() {
+        return breaker() +
+                "<button class=\"w-100 btn btn-primary btn-lg\" type=\"submit\">" +
+                SUBMIT_BUTTON_DEFAULT_TEXT +
+                "</button>";
+    }
+
+    public static String divContainer(String containerStr) {
+        return "<div class=\"container mt-5 mb-5\">" + containerStr + "</div>";
+    }
+
+    public static String form(String pageToSend, String method, String formItems, String submitButton) {
+        return "<form action=\"/" + pageToSend + "\" method=\"" + method + "\">\n" +
+                formItems +
+                submitButton +
+                "</form>";
+    }
+
+    public static String split() {
+        return "<hr class=\"my-4\">";
+    }
+
+    public static String breaker() {
+        return "<br>";
+    }
+}
--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/ChooseTypeOfWorkController.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/ChooseTypeOfWorkController.java
@@ -2,30 +2,66 @@ package com.vshmaliukh.springwebappmodule.conrollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.vshmaliukh.ConfigFile;
 
+import java.util.Map;
+
+import static com.vshmaliukh.springwebappmodule.BootstrapHtmlBuilder.*;
 import static com.vshmaliukh.springwebappmodule.SpringWebAppModuleApplication.*;
+import static com.vshmaliukh.springwebappmodule.conrollers.CookieController.COOKIE_TITLE;
+import static com.vshmaliukh.springwebappmodule.conrollers.CookieController.PAGE_TO_REDIRECT;
+import static org.vshmaliukh.Constants.MAIN_MENU_TITLE;
+import static org.vshmaliukh.Constants.TYPE_OF_WORK_WITH_FILES;
 
 @Controller
 public class ChooseTypeOfWorkController {
 
+    public static final String DESCRIPTION = "Choose type of work to save/read information about bookshelf items";
+
     @GetMapping("/" + CHOOSE_TYPE_OF_WORK_TITLE)
-    ModelAndView doGet(ModelMap modelMap) {
+    ModelAndView doGet(@CookieValue(value = TYPE_OF_WORK_WITH_FILES, defaultValue = "") String typeOfWork,
+                       ModelMap modelMap) {
         // TODO add implementation code
 
-        String generatedHtmlStr = "";
-        modelMap.addAttribute(GENERATED_HTML_STR, generatedHtmlStr);
+        StringBuilder sb = new StringBuilder();
+        sb.append(description(DESCRIPTION));
+        sb.append(split());
+        sb.append(divContainer(
+                form(CHOOSE_TYPE_OF_WORK_TITLE, "post", generateRadioButtons(typeOfWork), formSubmitButton())
+        ));
+
+        modelMap.addAttribute(GENERATED_HTML_STR, sb.toString());
         return new ModelAndView(BASE_PAGE_WITH_PLACEHOLDER, modelMap);
     }
 
-    @PostMapping("/" + CHOOSE_TYPE_OF_WORK_TITLE)
-    ModelAndView doPost(ModelMap modelMap) {
-        // TODO add implementation code
+    String generateRadioButtons(String currentTypeOfWork) {
+        StringBuilder sb = new StringBuilder();
+        Map<Object, String> typeOfWorkMap = ConfigFile.typeOfWorkMap;
+        for (Map.Entry<Object, String> entry : typeOfWorkMap.entrySet()) {
+            String integerTypeOfWork = entry.getKey().toString();
+            String friendlyStrTypeOfWork = entry.getValue();
+            if(currentTypeOfWork.equals(integerTypeOfWork)){
+                sb.append(radioButton(friendlyStrTypeOfWork, integerTypeOfWork, TYPE_OF_WORK_WITH_FILES, true));
+            }
+            else {
+                sb.append(radioButton(friendlyStrTypeOfWork, integerTypeOfWork, TYPE_OF_WORK_WITH_FILES, false));
+            }
 
-        return new ModelAndView(
-//                "redirect:/" +
-                BASE_PAGE_WITH_PLACEHOLDER, modelMap);
+        }
+        return sb.toString();
+    }
+
+    @PostMapping("/" + CHOOSE_TYPE_OF_WORK_TITLE)
+    ModelAndView doPost(@RequestParam Map<String, String> allParams,
+                        ModelMap modelMap) {
+
+        modelMap.addAttribute(TYPE_OF_WORK_WITH_FILES, allParams.get(TYPE_OF_WORK_WITH_FILES));
+        modelMap.addAttribute(PAGE_TO_REDIRECT, MAIN_MENU_TITLE);
+        return new ModelAndView("redirect:/" + COOKIE_TITLE, modelMap);
     }
 }
