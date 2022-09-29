af5c19a758fd03d1feed62f42109b8f5f35c4043
vshmaliukh <zskat02@gmail.com>
2022-09-29 17:23:24 +0300

    Created spring boot entity classes according to existing Mysql bd

---
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/entities/NewspaperEntity.java
@@ -0,0 +1,33 @@
+package com.vshmaliukh.springwebappmodule.shelf.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static com.vshmaliukh.springwebappmodule.shelf.ConstantsForDataBase.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name= MAGAZINE_TABLE)
+public class NewspaperEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = ID_COLUMN, nullable = false)
+    private Long id;
+
+    @Column(name = USER_ID_COLUMN, nullable = false)
+    private Long userId;
+
+    @Column(name = NAME_COLUMN, nullable = false)
+    String name;
+
+    @Column(name = PAGES_COLUMN, nullable = false)
+    int pages;
+
+    @Column(name = BORROWED_COLUMN, nullable = false)
+    boolean isBorrowed;
+
+}
---
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/entities/UserEntity.java
@@ -0,0 +1,24 @@
+package com.vshmaliukh.springwebappmodule.shelf.entities;
+
+import com.vshmaliukh.springwebappmodule.shelf.ConstantsForDataBase;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static com.vshmaliukh.springwebappmodule.shelf.ConstantsForDataBase.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = USER_TABLE)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = ConstantsForDataBase.ID_COLUMN, nullable = false)
+    private Long id;
+
+    @Column(name = USER_NAME_COLUMN, nullable = false)
+    private String userName;
+
+}
