1d3dc4af9068866eea5750f360c92752bbe6bc00
vshmaliukh <zskat02@gmail.com>
2022-09-30 17:01:33 +0300

    Refactored generating of radio buttons

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/ItemsSortingMenuController.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/ItemsSortingMenuController.java
@@ -6,27 +6,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.vshmaliukh.Constants;
+import org.vshmaliukh.services.menus.menu_items.MenuItemForSorting;
+import org.vshmaliukh.shelf.literature_items.ItemHandler;
 import org.vshmaliukh.shelf.literature_items.ItemHandlerProvider;
 import org.vshmaliukh.utils.UrlUtil;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static com.vshmaliukh.springwebappmodule.SpringWebAppModuleApplication.*;
+import static com.vshmaliukh.springwebappmodule.SpringWebAppModuleApplication.GENERATED_HTML_STR;
+import static com.vshmaliukh.springwebappmodule.SpringWebAppModuleApplication.GENERATED_TITTLE;
 import static org.vshmaliukh.BootstrapHtmlBuilder.*;
 import static org.vshmaliukh.Constants.*;
 
 @Controller
-@RequestMapping("/" + Constants.ITEMS_SORTING_MENU_TITLE)
+@RequestMapping("/" + ITEMS_SORTING_MENU_TITLE)
 public class ItemsSortingMenuController {
 
     @PostMapping()
     ModelAndView doPost(@RequestParam String menuItemIndex,
                         @RequestParam String itemClassType,
                         ModelMap modelMap) {
-        modelMap.addAttribute(Constants.MENU_ITEM_INDEX, menuItemIndex);
+        modelMap.addAttribute(MENU_ITEM_INDEX, menuItemIndex);
         modelMap.addAttribute(ITEM_CLASS_TYPE, itemClassType);
-        return new ModelAndView("redirect:/" + Constants.ITEMS_SORTING_MENU_TITLE, modelMap);
+        return new ModelAndView("redirect:/" + ITEMS_SORTING_MENU_TITLE, modelMap);
     }
 
     @GetMapping()
@@ -36,14 +40,28 @@ public class ItemsSortingMenuController {
                        @RequestParam(defaultValue = "") String itemClassType,
                        ModelMap modelMap) {
         Map<String, String> userAtr = ControllerUtils.adaptUserAtrToWebAppStandard(userName, typeOfWork);
-        String generateItemsTableStr = ControllerUtils.generateItemsTableStr(userAtr, menuItemIndex, itemClassType, ItemHandlerProvider.getHandlerByName(itemClassType));
+        String generateItemsTableStrOLD = ControllerUtils.generateItemsTableStr(userAtr, menuItemIndex, itemClassType, ItemHandlerProvider.getHandlerByName(itemClassType));
 
-        modelMap.addAttribute(GENERATED_HTML_STR, generatePageHtmlText(itemClassType, generateItemsTableStr));
+        ControllerUtils.formItemTableByClass(userName, typeOfWork, itemClassType, menuItemIndex, modelMap);
+//        ControllerUtils.formRadioButtonsToSortItemsByValue(itemClassType, modelMap);
+
+        Map<String, String> radioButtonsMap = new HashMap<>();
+        ItemHandler handlerByName = ItemHandlerProvider.getHandlerByName(itemClassType);
+        List<MenuItemForSorting> sortingMenuList = handlerByName.getSortingMenuList();
+        for (MenuItemForSorting menuItemForSorting : sortingMenuList) {
+            radioButtonsMap.put(String.valueOf(menuItemForSorting.getIndex()), menuItemForSorting.getStr());
+        }
+        modelMap.addAttribute("radioButtons", radioButtonsMap);
+
+
+
+        modelMap.addAttribute(ITEM_CLASS_TYPE, itemClassType);
+        modelMap.addAttribute(GENERATED_HTML_STR, generatePageHtmlText(itemClassType, generateItemsTableStrOLD));
         modelMap.addAttribute(GENERATED_TITTLE, itemClassType + " sorting");
-        return new ModelAndView(BASE_PAGE_WITH_PLACEHOLDER, modelMap);
+        return new ModelAndView(ITEMS_SORTING_MENU_TITLE, modelMap);
     }
 
-    private static String generatePageHtmlText(String itemClassType, String generateItemsTableStr) { // FIXME create template
+    private static String generatePageHtmlText(String itemClassType, String generateItemsTableStr) {
         StringBuilder sb = new StringBuilder();
         String generatedMenu = ControllerUtils.generateRadioButtonsMenuHtmlStr(ItemHandlerProvider.getHandlerByName(itemClassType).getSortingMenuList());
         URIBuilder uriBuilder = UrlUtil.generateBaseURLBuilder(ITEMS_SORTING_MENU_TITLE).addParameter(ITEM_CLASS_TYPE, itemClassType);
--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/utils/ControllerUtils.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/utils/ControllerUtils.java
@@ -1,5 +1,6 @@
 package com.vshmaliukh.springwebappmodule.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.ui.ModelMap;
 import org.vshmaliukh.ConfigFile;
 import org.vshmaliukh.print_table_service.TableGenerator;
@@ -7,16 +8,14 @@ import org.vshmaliukh.services.ConvertorToStringForItems;
 import org.vshmaliukh.services.SaveReadShelfHandler;
 import org.vshmaliukh.services.menus.menu_items.MenuItem;
 import org.vshmaliukh.services.menus.menu_items.MenuItemClassType;
+import org.vshmaliukh.services.menus.menu_items.MenuItemForSorting;
 import org.vshmaliukh.shelf.Shelf;
 import org.vshmaliukh.shelf.literature_items.Item;
 import org.vshmaliukh.shelf.literature_items.ItemHandler;
 import org.vshmaliukh.shelf.literature_items.ItemHandlerProvider;
 import org.vshmaliukh.shelf.literature_items.ItemTitles;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.vshmaliukh.BootstrapHtmlBuilder.radioButton;
 import static org.vshmaliukh.ConfigFile.typeOfWorkMap;
@@ -39,10 +38,9 @@ public class ControllerUtils {
         for (Map.Entry<Object, String> entry : typeOfWorkMap.entrySet()) {
             String integerTypeOfWork = entry.getKey().toString();
             String friendlyStrTypeOfWork = entry.getValue();
-            if(currentTypeOfWork.equals(integerTypeOfWork)){
+            if (currentTypeOfWork.equals(integerTypeOfWork)) {
                 sb.append(radioButton(friendlyStrTypeOfWork, integerTypeOfWork, TYPE_OF_WORK_WITH_FILES, true));
-            }
-            else {
+            } else {
                 sb.append(radioButton(friendlyStrTypeOfWork, integerTypeOfWork, TYPE_OF_WORK_WITH_FILES, false));
             }
         }
@@ -66,7 +64,7 @@ public class ControllerUtils {
         return generatedMenuBuilder.toString();
     }
 
-    public static  <T extends Item> String generateItemsTableStr(Map<String, String> userAtr, String menuIndexStr, String classTypeStr, ItemHandler<T> handlerByName) {
+    public static <T extends Item> String generateItemsTableStr(Map<String, String> userAtr, String menuIndexStr, String classTypeStr, ItemHandler<T> handlerByName) {
         Class classType = ItemHandlerProvider.getClassByName(classTypeStr);
         SaveReadShelfHandler webShelfHandler = generateShelfHandler(userAtr);
         List<T> sortedItemsByClass = new ArrayList<>();
@@ -86,7 +84,7 @@ public class ControllerUtils {
         SaveReadShelfHandler webShelfHandler = generateShelfHandler(userName, typeOfWork);
         List<String> titles = new ArrayList<>();
         List<List<String>> tableValues = new ArrayList<>();
-        if(webShelfHandler != null){
+        if (webShelfHandler != null) {
             Shelf shelf = webShelfHandler.getShelf();
             if (shelf != null) {
                 List<Item> allItems = shelf.getAllLiteratureObjects();
@@ -98,5 +96,41 @@ public class ControllerUtils {
         }
         modelMap.addAttribute(TITLES, titles);
         modelMap.addAttribute(ITEMS, tableValues);
+    }
+
+    public static void formItemTableByClass(String userName, int typeOfWork, String itemClassType, String menuItemIndex, ModelMap modelMap) {
+        SaveReadShelfHandler webShelfHandler = generateShelfHandler(userName, typeOfWork);
+        List<String> titles = new ArrayList<>();
+        List<List<String>> tableValues = new ArrayList<>();
+        if (webShelfHandler != null) {
+            Shelf shelf = webShelfHandler.getShelf();
+            if (shelf != null) {
+                Class<? extends Item> classByName = ItemHandlerProvider.getClassByName(itemClassType);
+                List<? extends Item> itemsByClass = webShelfHandler.getSortedItemsByClass(classByName);
+
+                if (StringUtils.isNotBlank(menuItemIndex)) {
+                    int typeOfSorting = Integer.parseInt(menuItemIndex);
+                    ItemHandler handlerByClass = ItemHandlerProvider.getHandlerByClass(classByName);
+                    itemsByClass = handlerByClass.getSortedItems(typeOfSorting, itemsByClass);
+                }
+                List<Map<String, String>> table = ConvertorToStringForItems.getTable(itemsByClass);
+                TableGenerator tableGenerator = new TableGenerator(table, ItemTitles.TITLE_LIST, true);
+                titles.addAll(tableGenerator.getGeneratedTitleList());
+                tableValues.addAll(tableGenerator.getGeneratedTableList());
+            }
+        }
+        System.out.println(titles);
+        modelMap.addAttribute(TITLES, titles);
+        modelMap.addAttribute(ITEMS, tableValues);
+    }
+
+    public static void formRadioButtonsToSortItemsByValue(String itemClassType, ModelMap modelMap) {
+        Map<String, String> radioButtonsMap = new HashMap<>();
+        ItemHandler handlerByName = ItemHandlerProvider.getHandlerByName(itemClassType);
+        List<MenuItemForSorting> sortingMenuList = handlerByName.getSortingMenuList();
+        for (MenuItemForSorting menuItemForSorting : sortingMenuList) {
+            radioButtonsMap.put(String.valueOf(menuItemForSorting.getIndex()), menuItemForSorting.getStr());
+        }
+        modelMap.addAttribute("radioButtons", radioButtonsMap);
     }
 }
