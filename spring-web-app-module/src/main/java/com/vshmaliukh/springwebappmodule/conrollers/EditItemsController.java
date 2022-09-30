07b20970065e6bb4f23f391c389d6c0f79e86217
vshmaliukh <zskat02@gmail.com>
2022-09-30 14:15:15 +0300

    Refactored generating of literature items table for 'edit items' page Use template generating

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/EditItemsController.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/conrollers/EditItemsController.java
@@ -7,12 +7,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.vshmaliukh.utils.WebUtils;
 
-import java.util.Map;
-
-import static org.vshmaliukh.Constants.GENERATED_TABLE_HTML_STR;
-import static org.vshmaliukh.ConfigFile.typeOfWorkMap;
 import static org.vshmaliukh.Constants.EDIT_ITEMS_TITLE;
 import static org.vshmaliukh.Constants.TYPE_OF_WORK_WITH_FILES;
 
@@ -24,10 +19,9 @@ public class EditItemsController {
     ModelAndView doGet(@CookieValue String userName,
                        @CookieValue int typeOfWork,
                        ModelMap modelMap) {
-        Map<String, String> userAtr = ControllerUtils.adaptUserAtrToWebAppStandard(userName, typeOfWork); // TODO refactor
+        ControllerUtils.formCurrentStateTable(userName, typeOfWork, modelMap);
 
-        modelMap.addAttribute(TYPE_OF_WORK_WITH_FILES, typeOfWorkMap.get(typeOfWork));
-        modelMap.addAttribute(GENERATED_TABLE_HTML_STR, WebUtils.generateTableForEditingItems(userAtr));
+        modelMap.addAttribute(TYPE_OF_WORK_WITH_FILES, ControllerUtils.getFriendlyTypeOfWorkStr(typeOfWork));
         return new ModelAndView(EDIT_ITEMS_TITLE, modelMap);
     }
 
