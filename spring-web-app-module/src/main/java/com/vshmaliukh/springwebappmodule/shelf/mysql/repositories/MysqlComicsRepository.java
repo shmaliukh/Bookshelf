d691ed2e8c540a9793686508aeb8b472f0035573
vshmaliukh <zskat02@gmail.com>
2022-10-05 13:13:50 +0300

    Added '@Repository' annotation for mysql and sqlite repository classes

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/mysql/repositories/MysqlComicsRepository.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/mysql/repositories/MysqlComicsRepository.java
@@ -2,6 +2,8 @@ package com.vshmaliukh.springwebappmodule.shelf.mysql.repositories;
 
 import com.vshmaliukh.springwebappmodule.shelf.entities.ComicsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MysqlComicsRepository extends JpaRepository<ComicsEntity, Integer> {
 }
