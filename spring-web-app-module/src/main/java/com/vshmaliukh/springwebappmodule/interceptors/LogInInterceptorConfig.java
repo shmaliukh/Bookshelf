64c11ed9770659c3b49c725bdc11a831e06449e4
vshmaliukh <zskat02@gmail.com>
2022-10-13 12:59:23 +0300

    Cleaned up LogInInterceptorConfig code

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/interceptors/LogInInterceptorConfig.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/interceptors/LogInInterceptorConfig.java
@@ -1,6 +1,5 @@
 package com.vshmaliukh.springwebappmodule.interceptors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +9,11 @@ import static org.vshmaliukh.Constants.LOG_IN_TITLE;
 @Component
 public class LogInInterceptorConfig implements WebMvcConfigurer {
 
-    @Autowired
-    LogInInterceptor logInInterceptor;
+    final LogInInterceptor logInInterceptor;
+
+    public LogInInterceptorConfig(LogInInterceptor logInInterceptor) {
+        this.logInInterceptor = logInInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
