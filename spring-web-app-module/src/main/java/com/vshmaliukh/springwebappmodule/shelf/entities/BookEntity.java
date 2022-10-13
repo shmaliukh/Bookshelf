4aad4d6e3b901ead9e8ac6493489d28f8f3af944
vshmaliukh <zskat02@gmail.com>
2022-10-13 10:36:38 +0300

    Use java.util.Date for book entity Deleted unnecessary lombok annotations to build book entity constructors

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/entities/BookEntity.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/shelf/entities/BookEntity.java
@@ -1,25 +1,26 @@
 package com.vshmaliukh.springwebappmodule.shelf.entities;
 
 import com.vshmaliukh.springwebappmodule.shelf.ConstantsForDataBase;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = ConstantsForDataBase.BOOK_TABLE)
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookEntity extends ItemEntity {
 
     @Column(name = ConstantsForDataBase.AUTHOR_COLUMN, nullable = false)
     String author;
 
     @Column(name = ConstantsForDataBase.DATE_COLUMN, nullable = false)
-    java.sql.Date dateOfIssue;
+    Date dateOfIssue;
 
 }
