14792dfe838f0fc555122b1f644bb5e09efa9bbf
vshmaliukh <zskat02@gmail.com>
2022-10-19 10:09:49 +0300

    Added spring app console test block

---
+++ spring-console-app-module/src/main/java/com/vshmaliukh/springconsoleappmodule/SpringConsoleAppModuleApplication.java
@@ -0,0 +1,14 @@
+package com.vshmaliukh.springconsoleappmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringConsoleAppModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConsoleAppModuleApplication.class, args);
+    }
+
+}
