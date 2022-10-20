28897767c8efd6db9acb756d0777a12f0cfb326d
vshmaliukh <zskat02@gmail.com>
2022-10-20 13:46:59 +0300

    Configure spring web app module
    Cleaned up pom file dependency duplicate
    Added 'scanBasePackages' values to scan beans from core and web app modules

--- spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/SpringWebAppModuleApplication.java
+++ spring-web-app-module/src/main/java/com/vshmaliukh/springwebappmodule/SpringWebAppModuleApplication.java
@@ -4,13 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.vshmaliukh.springbootshelfcore.shelf" ,
-        "com.vshmaliukh.springbootshelfcore.shelf.convertors",
-        "com.vshmaliukh.springbootshelfcore.shelf.entities",
-        "com.vshmaliukh.springbootshelfcore.shelf.mysql",
-        "com.vshmaliukh.springbootshelfcore.shelf.repository_services",
-        "com.vshmaliukh.springbootshelfcore.shelf.sqlite",
-        "com.vshmaliukh.*"})
+        "com.vshmaliukh.springbootshelfcore",
+        "com.vshmaliukh.springwebappmodule",
+})
 public class SpringWebAppModuleApplication {
 
     public static void main(String[] args) {
