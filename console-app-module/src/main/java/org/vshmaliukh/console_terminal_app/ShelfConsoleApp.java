637234b453eb22ae75b54c8054d4ea69c8b1e145
vshmaliukh <zskat02@gmail.com>
2022-10-20 10:49:00 +0300

    Cleaned up ShelfConsoleApp class

--- console-app-module/src/main/java/org/vshmaliukh/console_terminal_app/ShelfConsoleApp.java
+++ console-app-module/src/main/java/org/vshmaliukh/console_terminal_app/ShelfConsoleApp.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 
 public class ShelfConsoleApp {
 
-    public static final String MODULE_NAME = "console-app-module";
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         PrintWriter printWriter = new PrintWriter(System.out, true);
 
         ConsoleUI consoleUI = new ConsoleUI(scanner, printWriter);
-
         consoleUI.startWork(true);
     }
+
 }
