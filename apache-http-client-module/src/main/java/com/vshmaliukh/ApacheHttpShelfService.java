55d116eb4da7a3b7c4a1a0944581e068aa6e34b5
vshmaliukh <zskat02@gmail.com>
2022-11-03 14:45:06 +0200

    Implement methods to handle items via sending http requests to spring boot web app server
    Implemented 'saveItemToDB' method
    Start to implement 'deleteItemFromDB' method
    Use logIn controller response to set up base cookies (userName and typeOfWork)
    Use handling for cookies instead of CookieStore

--- apache-http-client-module/src/main/java/com/vshmaliukh/ApacheHttpShelfService.java
+++ apache-http-client-module/src/main/java/com/vshmaliukh/ApacheHttpShelfService.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.Cookie;
 import org.apache.hc.client5.http.cookie.CookieStore;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
@@ -14,6 +14,7 @@ import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.net.URIBuilder;
 import org.vshmaliukh.MyLogUtil;
 import org.vshmaliukh.UserDataModelForJson;
 import org.vshmaliukh.services.save_read_services.sql_handler.AbstractSqlHandler;
@@ -23,24 +24,27 @@ import org.vshmaliukh.shelf.literature_items.ItemHandlerProvider;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.vshmaliukh.MyApacheUtils.COOKIE_HEADER;
+
 public class ApacheHttpShelfService extends AbstractSqlHandler {
 
-    public static final String USER_NAME = "userName";
-    public static final String TYPE_OF_WORK = "typeOfWork";
-    public static final String ITEM_CLASS_TYPE = "itemClassType";
+    public static final String HTTP_LOCALHOST_8082 = "http://localhost:8082/";
+
     public static final String APPLICATION_JSON = "application/json";
     public static final String INDEX_OF_ITEM = "indexOfItem";
     public static final String CLASS_TYPE = "classType";
+    public static final String ITEM_CLASS_TYPE = "itemClassType";
 
     protected int typeOfWork;
+    protected List<String> cookieValueList = new ArrayList<>();
+
     protected Gson gson = new Gson();
-    protected CookieStore cookieStore = new BasicCookieStore();
-    protected CloseableHttpClient client = HttpClientBuilder.create()
-            .setDefaultCookieStore(cookieStore)
-            .build();
+
+    protected CloseableHttpClient client = HttpClientBuilder.create().build();
 
     protected ApacheHttpShelfService(String userName, int typeOfWork) {
         super(userName);
@@ -49,22 +53,24 @@ public class ApacheHttpShelfService extends AbstractSqlHandler {
     }
 
     private void logIn(String userName, int typeOfWork) {
-        HttpPost post = new HttpPost("http://localhost:8082/log_in");
+        HttpPost httpPost = new HttpPost(HTTP_LOCALHOST_8082 + "log_in");
         HttpClientContext context = HttpClientContext.create();
         try {
             UserDataModelForJson userDataModelForJson = new UserDataModelForJson(userName, typeOfWork);
             String userGsonStr = gson.toJson(userDataModelForJson);
             StringEntity stringEntity = new StringEntity(userGsonStr);
-            post.setEntity(stringEntity);
-            post.setHeader("Accept", "application/json");
-            post.setHeader("Content-type", "application/json");
-            CloseableHttpResponse response = client.execute(post, context);
+            httpPost.setEntity(stringEntity);
+            httpPost.addHeader("Accept", APPLICATION_JSON);
+            httpPost.addHeader("Content-type", APPLICATION_JSON);
+            CloseableHttpResponse response = client.execute(httpPost, context);
             for (Header header : response.getHeaders()) {
                 System.out.println(header.getName() + " = " + header.getValue());
             }
-            cookieStore = context.getCookieStore();
-            System.out.println(cookieStore.getCookies());
-            MyLogUtil.logInfo(this, "user: '{}', typeOfWork: '{}' // cookies: '{}'", userName, typeOfWork, cookieStore.getCookies());
+            CookieStore cookieStore = context.getCookieStore();
+            for (Cookie cookie : cookieStore.getCookies()) {
+                cookieValueList.add(MyApacheUtils.generateCookieValue(cookie.getName(), cookie.getValue()));
+            }
+            MyLogUtil.logDebug(this, "userNamed: '{}', typeOfWork: '{}' // cookies: '{}'", userName, typeOfWork, cookieStore.getCookies());
         } catch (IOException e) {
             MyLogUtil.logErr(this, e);
         }
@@ -81,22 +87,46 @@ public class ApacheHttpShelfService extends AbstractSqlHandler {
 
     @Override
     public void deleteItemFromDB(Item item) {
-
+        HttpGet httpGet = new HttpGet(HTTP_LOCALHOST_8082 + "delete_item");
+        cookieValueList.forEach(o -> httpGet.addHeader(COOKIE_HEADER, o));
+        try {
+            int index = readItemList().indexOf(item) + 1;
+            System.out.println("item to delete: " + item + " // index: " + index);
+            URI uri = new URIBuilder(httpGet.getUri())
+                    .addParameter(INDEX_OF_ITEM, String.valueOf(index))
+                    .build();
+            httpGet.setUri(uri);
+            CloseableHttpResponse httpResponse = client.execute(httpGet);
+            System.out.println(httpResponse);
+        } catch (Exception e) {
+            MyLogUtil.logErr(this, e);
+        }
     }
 
     @Override
     public void changeItemBorrowedStateInDB(Item item) {
-        HttpGet httpGet = new HttpGet("http://localhost:8082/change_item_borrowed_state");
+        HttpGet httpGet = new HttpGet(HTTP_LOCALHOST_8082 + "change_item_borrowed_state");
+        cookieValueList.forEach(o -> httpGet.addHeader(COOKIE_HEADER, o));
+        try {
+            int index = readItemList().indexOf(item) + 1;
+            URI uri = new URIBuilder(httpGet.getUri())
+                    .addParameter(INDEX_OF_ITEM, String.valueOf(index))
+                    .build();
+            httpGet.setUri(uri);
+            CloseableHttpResponse httpResponse = client.execute(httpGet);
+            System.out.println(httpResponse);
+        } catch (Exception e) {
+            MyLogUtil.logErr(this, e);
+        }
     }
 
     @Override
     public <T extends Item> List<T> readItemsByClass(Class<T> classType) {
         String classTypeStr = classType.getSimpleName();
-        HttpGet httpGet = new HttpGet("http://localhost:8082/readItemsByType");
+        HttpGet httpGet = new HttpGet(HTTP_LOCALHOST_8082 + "readItemsByType");
         httpGet.addHeader("Content-Type", APPLICATION_JSON);
-        httpGet.addHeader("Cookie", USER_NAME+"="+userName);
-        httpGet.addHeader("Cookie", TYPE_OF_WORK+"="+typeOfWork);
-        httpGet.addHeader("Cookie", ITEM_CLASS_TYPE+"="+classTypeStr);
+        cookieValueList.forEach(o -> httpGet.addHeader(COOKIE_HEADER, o));
+        httpGet.addHeader(COOKIE_HEADER, CLASS_TYPE + "=" + classTypeStr);
         return readItemListByClassFromResponse(httpGet, classType);
     }
 
@@ -114,7 +144,18 @@ public class ApacheHttpShelfService extends AbstractSqlHandler {
 
     @Override
     public void saveItemToDB(Item item) {
-
+        String itemClassTypeStr = item.getClass().getSimpleName();
+        HttpPost httpPost = new HttpPost(HTTP_LOCALHOST_8082 + "add_item");
+        cookieValueList.forEach(o -> httpPost.addHeader(COOKIE_HEADER, o));
+        httpPost.addHeader(COOKIE_HEADER, MyApacheUtils.generateCookieValue(ITEM_CLASS_TYPE, itemClassTypeStr));
+        String userGsonStr = gson.toJson(item);
+        StringEntity stringEntity = new StringEntity(userGsonStr);
+        httpPost.setEntity(stringEntity);
+        try {
+            client.execute(httpPost);
+        } catch (Exception e) {
+            MyLogUtil.logErr(this, e);
+        }
     }
 
     @Override
